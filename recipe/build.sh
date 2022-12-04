d98a0d6e6a3ece8afdc907a17b326f7df1449cef
Mark Harfouche <mark.harfouche@gmail.com>
2022-12-04 14:38:20 -0500

    Add mpich flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,19 @@
+set -ex
 mkdir -p build
 cd build
-cmake .. ${CMAKE_ARGS} -DUSE_MKL=OFF -DUSE_SCALAPACK=ON -DUSE_OPENMP=ON -DCREATE_PYTHON_MODULE=OFF -DCREATE_FORTRAN_BINDINGS=ON
+
+if [[ ${mpi} == "mpich" ]]; then
+   # Allow argument mismatch in Fortran
+   # https://github.com/pmodels/mpich/issues/4300
+   export FFLAGS="$FFLAGS -fallow-argument-mismatch"
+   export FCFLAGS="$FCFLAGS -fallow-argument-mismatch"
+fi
+cmake .. \
+  ${CMAKE_ARGS} \
+  -DUSE_MKL=OFF \
+  -DUSE_SCALAPACK=ON \
+  -DUSE_OPENMP=ON \
+  -DCREATE_PYTHON_MODULE=OFF \
+  -DCREATE_FORTRAN_BINDINGS=ON
+
 make -j${CPU_COUNT} install
